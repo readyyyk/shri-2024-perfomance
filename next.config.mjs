b8cc589c3e88ad6d34c5f7b9d25484056c11c37c
readyyyk <mr.sairexer@gmail.com>
2024-07-10 16:35:08 +0300

    moving assets 1

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     basePath: "/shri-2024-perfomance",
-    assetPrefix: "/shri-2024-perfomance",
+    assetPrefix: "./shri-2024-perfomance/",
 };
 
 export default nextConfig;

0e3875aa283acf423bdbcec3a5e1b82802cb884a
readyyyk <mr.sairexer@gmail.com>
2024-07-12 09:17:23 +0300

    try webp

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-import million from "million/compiler";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: "/shri-2024-perfomance"
 };
-export default million.next(nextConfig);
+
+export default nextConfig;
